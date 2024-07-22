27491d32c2d4dedbf17dabb5b59c7eacfced53d3
hammadx02 <hammadaali02@gmail.com>
2024-07-22 22:24:07 +0500

    adding animations

--- lib/example_1.dart
+++ lib/example_1.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 class Example1 extends StatefulWidget {
@@ -7,9 +8,42 @@ class Example1 extends StatefulWidget {
   State<Example1> createState() => _Example1State();
 }
 
-class _Example1State extends State<Example1> {
+class _Example1State extends State<Example1>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+    _animation = Tween<double>(begin: 0.0, end: 2 * pi).animate(_controller);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Container(
+          width: 100,
+          height: 100,
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.5),
+                spreadRadius: 5,
+                blurRadius: 7,
+                offset: const Offset(0, 3),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
