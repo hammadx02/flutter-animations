5a8fb9334e1aa9a6b242106aa1701427cd56bd4f
hammadx02 <hammadaali02@gmail.com>
2024-07-23 14:44:00 +0500

    example 2 added

---
+++ lib/example_2.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Example2 extends StatefulWidget {
+  const Example2({super.key});
+
+  @override
+  State<Example2> createState() => _Example2State();
+}
+
+class _Example2State extends State<Example2> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
