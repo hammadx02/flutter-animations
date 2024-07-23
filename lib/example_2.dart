6c9ed6a8cd09974fc8bfd0d0dd1c0238aa2ef525
hammadx02 <hammadaali02@gmail.com>
2024-07-23 14:56:12 +0500

    updated

--- lib/example_2.dart
+++ lib/example_2.dart
@@ -7,9 +7,52 @@ class Example2 extends StatefulWidget {
   State<Example2> createState() => _Example2State();
 }
 
+enum CircleSide {
+  left,
+  right,
+}
+
+extension ToPath on CircleSide {
+  Path toPath(Size size) {
+    final path = Path();
+    late Offset offset;
+    late bool clockwise;
+
+    switch (this) {
+      case CircleSide.left:
+        path.moveTo(size.width, 0);
+        offset = Offset(size.width, size.height);
+        clockwise = false;
+        break;
+      case CircleSide.right:
+        offset = Offset(0, size.height);
+        clockwise = true;
+        break;
+    }
+  }
+}
+
 class _Example2State extends State<Example2> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              color: const Color(0xff0057b7),
+              width: 150,
+              height: 150,
+            ),
+            Container(
+              color: const Color(0xffffd700),
+              width: 150,
+              height: 150,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
