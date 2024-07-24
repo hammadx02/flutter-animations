1201a7f22cbafbec2ec71f9116e891aafa38e12f
hammadx02 <hammadaali02@gmail.com>
2024-07-24 14:25:56 +0500

    updated

--- lib/screens/sandbox.dart
+++ lib/screens/sandbox.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Sandbox extends StatefulWidget {
   const Sandbox({super.key});
@@ -8,8 +9,39 @@ class Sandbox extends StatefulWidget {
 }
 
 class _SandboxState extends State<Sandbox> {
+  double _opacity = 1;
+  double _margin = 20;
+  double _width = 200;
+  Color _color = Colors.blue;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: AnimatedContainer(
+        duration: const Duration(
+          seconds: 1,
+        ),
+        margin: EdgeInsets.all(_margin),
+        width: _width,
+        color: _color,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                setState(
+                  () {
+                    _margin = 50;
+                  },
+                );
+              },
+              child: const Text(
+                'Animate margin',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
