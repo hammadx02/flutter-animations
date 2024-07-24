3505b9bb99b3727fda2925aaad15ee1ed58ca16d
hammadx02 <hammadaali02@gmail.com>
2024-07-24 14:19:40 +0500

    adding animations

---
+++ lib/screens/sandbox.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Sandbox extends StatefulWidget {
+  const Sandbox({super.key});
+
+  @override
+  State<Sandbox> createState() => _SandboxState();
+}
+
+class _SandboxState extends State<Sandbox> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
