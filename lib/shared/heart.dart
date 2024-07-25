8695c593c04c7ee4157741c1eb7d188e4ddcef79
hammadx02 <hammadaali02@gmail.com>
2024-07-25 10:57:44 +0500

    updated

--- lib/shared/heart.dart
+++ lib/shared/heart.dart
@@ -5,13 +5,30 @@ class Heart extends StatefulWidget {
   _HeartState createState() => _HeartState();
 }
 
-class _HeartState extends State<Heart> {
+class _HeartState extends State<Heart> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation _colorAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+
+    _colorAnimation = ColorTween(
+      begin: Colors.grey[400],
+      end: Colors.red,
+    ).animate(_controller);
+  }
+
   @override
   Widget build(BuildContext context) {
     return IconButton(
       icon: Icon(
         Icons.favorite,
-        color: Colors.grey[400],
+        color: _colorAnimation.value,
         size: 30,
       ),
       onPressed: () {},
